e094834d5265e46771d38c553d0f4aa603d3728c
WestleyK <westley@sylabs.io>
2019-09-21 20:02:23 -0700

    Support for 2 7-segment displays

--- arduino-speedOmeter_v1/arduino-speedOmeter_v1.ino
+++ arduino-speedOmeter_v1/arduino-speedOmeter_v1.ino
@@ -3,7 +3,7 @@
 // Date: Sep 21, 2019
 // Repo: https://github.com/WestleyR/arduino-speedOmeter
 //
-// Version: 1.0.0-beta-1
+// Version: 1.0.0-beta-2
 //
 // License: The Clear BSD License
 //
@@ -12,26 +12,33 @@
 //
 // This software is licensed under a Clear BSD License.
 //
+//
+// This project is designed to take a input pulse (like from a spinning wheel)
+// and convert those pulses to MPH, and display it on 2 (or 3) 7-segment LED
+// displays.
+//
 
-// BCD pins, these go to the bcd decoder/driver.
-#define BCD_1 10
-#define BCD_2 11
-#define BCD_3 12
-#define BCD_4 13
 
 // The output pins to the BCD decoder, in revers order.
-int BCD_OUTPUT[] = {13, 12, 11, 10};
+// BCD_A_OUTPUT = first 7-segment display,
+// BCD_B_OUTPUT = second 7-segment display.
+int BCD_A_OUTPUT[] = {13, 12, 11, 10};
+int BCD_B_OUTPUT[] = {9, 8, 7, 6};
 
-void writeBcdOutput(int out[]) {
+void writeBcdAOutput(int out[]) {
   for (int i = 0; i < 4; i++) {
-    digitalWrite(BCD_OUTPUT[i], out[i]);
+    digitalWrite(BCD_A_OUTPUT[i], out[i]);
+  }
+}
+
+void writeBcdBOutput(int out[]) {
+  for (int i = 0; i < 4; i++) {
+    digitalWrite(BCD_B_OUTPUT[i], out[i]);
   }
 }
 
 // This converts a int, to a BCD array. Eg,
 // 3 => [ 0 0 1 1 ].
-// NOTE: there will need to be 4 of these to
-// get the final output.
 int getBcdArray(int ret[], int msg) {
   for (int b = 0; b < 4; b++) {
     int rbyte = msg & (0x008 >> b);
@@ -42,24 +49,62 @@ int getBcdArray(int ret[], int msg) {
 }
 
 
-void writeOutput(int num) {
+// writeAOutput will write to display A.
+void writeAOutput(int num) {
   int outputArr[] = {0, 0, 0, 0};
 
   getBcdArray(outputArr, num);
-  writeBcdOutput(outputArr);
+  writeBcdAOutput(outputArr);
+}
+
+// writeAOutput will write to display B.
+void writeBOutput(int num) {
+  int outputArr[] = {0, 0, 0, 0};
+
+  getBcdArray(outputArr, num);
+  writeBcdBOutput(outputArr);
+}
+
+// splitInt will take a num, and split it into
+// a array.
+void splitInt(int arr[], int num) {
+  int i = 0;
+  while (num > 0) {
+    int mod = num % 10;
+    arr[i] = mod;
+    num = num / 10;
+    i++;
+  }
+}
+
+// writeAllOutput will take a num, and write
+// it to all displays.
+void writeAllOutput(int num) {
+  if (num > 9) {
+    int arr[2];
+
+    splitInt(arr, num);
+    writeBOutput(arr[0]);
+    writeAOutput(arr[1]);
+  } else {
+    // num is less then 10, can only use the last digit to print it.
+    writeBOutput(num);
+    writeAOutput(0);
+  }
 }
 
 void setup() {
-  pinMode(BCD_1, OUTPUT);
-  pinMode(BCD_2, OUTPUT);
-  pinMode(BCD_3, OUTPUT);
-  pinMode(BCD_4, OUTPUT);
+  // Declare BCD pins are outputs
+  for (int i = 0; i < 4; i++) {
+    pinMode(BCD_A_OUTPUT[i], OUTPUT);
+    pinMode(BCD_B_OUTPUT[i], OUTPUT);
+  }
 }
 
 void loop() {
   // As a test, write 0-9
-  for (int i = 0; i < 10; i++) {
-    writeOutput(i);
-    delay(500);
+  for (int i = 0; i < 90; i++) {
+    writeAllOutput(i);
+    delay(100);
   }
 }
