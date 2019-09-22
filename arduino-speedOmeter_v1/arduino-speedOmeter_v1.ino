9b4c81e4142c85597ac8ae5d50d5904c2a0f2246
WestleyK <westley@sylabs.io>
2019-09-22 11:44:17 -0700

    Inproved comments + better int BCD decoder

--- arduino-speedOmeter_v1/arduino-speedOmeter_v1.ino
+++ arduino-speedOmeter_v1/arduino-speedOmeter_v1.ino
@@ -1,9 +1,9 @@
 // Created by: WestleyK
 // Email: westleyk@nym.hush.com
-// Date: Sep 21, 2019
+// Date: Sep 22, 2019
 // Repo: https://github.com/WestleyR/arduino-speedOmeter
 //
-// Version: 1.0.0-beta-2
+// Version: 1.0.0-beta-3
 //
 // License: The Clear BSD License
 //
@@ -19,18 +19,27 @@
 //
 
 
-// The output pins to the BCD decoder, in revers order.
+// The output pins to the BCD decoder, in high to low.
 // BCD_A_OUTPUT = first 7-segment display,
 // BCD_B_OUTPUT = second 7-segment display.
+//  _   _   _
+// |_| |_| |_|
+// |_| |_| |_|
+//  A   B   C
+//
 int BCD_A_OUTPUT[] = {13, 12, 11, 10};
 int BCD_B_OUTPUT[] = {9, 8, 7, 6};
 
+// writeBcdAOutput will take a array, and
+// write it to the output pins for A.
 void writeBcdAOutput(int out[]) {
   for (int i = 0; i < 4; i++) {
     digitalWrite(BCD_A_OUTPUT[i], out[i]);
   }
 }
 
+// writeBcdAOutput will take a array, and
+// write it to the output pins for B.
 void writeBcdBOutput(int out[]) {
   for (int i = 0; i < 4; i++) {
     digitalWrite(BCD_B_OUTPUT[i], out[i]);
@@ -39,15 +48,19 @@ void writeBcdBOutput(int out[]) {
 
 // This converts a int, to a BCD array. Eg,
 // 3 => [ 0 0 1 1 ].
-int getBcdArray(int ret[], int msg) {
-  for (int b = 0; b < 4; b++) {
-    int rbyte = msg & (0x008 >> b);
-    ret[b] = rbyte ? 1 : 0;
+void getBcdArray(int ret[], int num) {
+  int rindex = 0;
+  for (int c = 3; c >= 0; c--) {
+    int k = num >> c;
+
+    if (k & 1) {
+      ret[rindex] = 1;
+    } else {
+      ret[rindex] = 0;
+    }
+    rindex++;
   }
-
-  return (0);
 }
-
 
 // writeAOutput will write to display A.
 void writeAOutput(int num) {
@@ -66,7 +79,7 @@ void writeBOutput(int num) {
 }
 
 // splitInt will take a num, and split it into
-// a array.
+// a array, but reversed. eg, 23 => [ 3 2 ].
 void splitInt(int arr[], int num) {
   int i = 0;
   while (num > 0) {
@@ -82,13 +95,14 @@ void splitInt(int arr[], int num) {
 void writeAllOutput(int num) {
   if (num > 9) {
     int arr[2];
-
     splitInt(arr, num);
     writeBOutput(arr[0]);
     writeAOutput(arr[1]);
   } else {
-    // num is less then 10, can only use the last digit to print it.
+    // num is less then 10, we can just use
+    // the last digit to print it.
     writeBOutput(num);
+    // Make sure the other diplays(s) are 0.
     writeAOutput(0);
   }
 }
@@ -103,7 +117,7 @@ void setup() {
 
 void loop() {
   // As a test, write 0-9
-  for (int i = 0; i < 90; i++) {
+  for (int i = 0; i < 99; i++) {
     writeAllOutput(i);
     delay(100);
   }
