f1725e23c7c8d79f31042db8bf241fb635595c94
WestleyK <westley@sylabs.io>
2019-10-11 17:05:51 -0700

    Fixing up v3, adding comments + updated code

--- arduino-speedOmeter_v3/arduino-speedOmeter_v3.ino
+++ arduino-speedOmeter_v3/arduino-speedOmeter_v3.ino
@@ -1,9 +1,9 @@
-// Created by: WestleyK
-// Email: westleyk@nym.hush.com
+// Created by: WestleyR
+// Email: westleyR@nym.hush.com
 // Date: Oct 12, 2019
 // Repo: https://github.com/WestleyR/arduino-speedOmeter
 //
-// Version: 1.0.0-beta-12
+// Version: 1.0.0-beta-14
 //
 // License: The Clear BSD License
 //
@@ -13,51 +13,52 @@
 // This software is licensed under a Clear BSD License.
 //
 //
-// The full project is designed to take a input pulse (like from a spinning wheel)
-// and convert those pulses to MPH, and display it on 2 (or 3) 7-segment LED
-// displays.
+// This project will take a pulse (from a spinning wheel) and
+// convert that to MPH (miles per hour), and also has a odometer
+// to print the total trip.
 //
-
 
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
-
+// Define the lcd and address.
 LiquidCrystal_I2C lcd(0x27, 16, 4);
 
-
-// The pulse input.
+// The pulse input from the wheel.
 #define SPEED_IN 7
 
+// This is the magic number, its the miles/per (2)? wheel
+// rotations (per/input pulse). Needs to be adjusted as
+// needed for your wheel size. ~This is for 8 inch wheel.~
+const float milesPerPulse = 0.000189394;
+
 // The refresh rate for the display, in milliseconds.
+// Recommend you leave as is.
 const unsigned long displayUpdateDelay = 500;
 
-// A couple of timer vars.
+// A couple of timer vars. Do not change!
 unsigned long updateTimer = 0;
 bool updateReset = false;
 
-// This is the magic number, its the miles/per wheel
-// rotation (per/input pulse). Needs to be adgusted
-// as needed for your wheel size.
-const float milesPerPulse = 0.000189394;
+// Display update timers. Do not change!
+unsigned long breakTimerStart;
+float currentMiles = 0;
+unsigned long displayUpdateRate = 0;
 
 void setup() {
   // The pulse input.
   pinMode(SPEED_IN, INPUT_PULLUP);
-
-  // TODO: tmp
-  pinMode(LED_BUILTIN, OUTPUT);
-  tone(LED_BUILTIN, 31);
 
   // Setup the lcd.
   lcd.init();
   lcd.backlight();
 }
 
-float currentMiles = 0;
-unsigned long displayUpdateRate = 0;
-
+// updateLcd will update the lcd, without loosing
+// the odometer trip.
 void updateLcd(float currentSpeed) {
+  // Clear the lcd before printing anything,
+  // this will also clear any stray chars.
   lcd.clear();
 
   // Print the MPH.
@@ -71,14 +72,16 @@ void updateLcd(float currentSpeed) {
   lcd.print("TOT");
   lcd.setCursor(0, 1);
 
+  // Get the live update rate, use that as our time references.
   unsigned long lastUpdate = millis() - displayUpdateRate;
+  long double mileSpeed = currentSpeed * lastUpdate;
+  currentMiles += mileSpeed;
 
-  long double tmp = currentSpeed * lastUpdate;
-  currentMiles += tmp;
-
+  // Divide the currentMiles by 3.6e+6 to get total miles,
+  // sinse currentMiles is in milliseconds.
   lcd.print(currentMiles / 3600000);
-//  lcd.print(lastUpdate);
 
+  // Reset the update counter.
   displayUpdateRate = millis();
 }
 
@@ -90,10 +93,8 @@ void calcSpeed(long double timePulse) {
   updateLcd(currentSpeed);
 }
 
-unsigned long breakTimerStart;
-
 bool timerBreak(unsigned long timer) {
-  if ((timer - breakTimerStart) >= (displayUpdateDelay * 2)) {
+  if ((timer - breakTimerStart) >= (displayUpdateDelay * 2.5)) {
     return (true);
   }
   return (false);
